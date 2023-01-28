a77c8c485ba20a9d433526b383d8057c745c06f2
kws9208 <kws9208@gmail.com>
2023-01-28 09:47:33 +0900

    [1주차] todo-list ERD 설계 및 DB 연동

---
+++ contents/seok/src/main/java/com/springboot/todo/entity/Todo.java
@@ -0,0 +1,28 @@
+package com.springboot.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean done;
+
+}
---
+++ contents/seok/src/main/java/com/springboot/todo/entity/User.java
@@ -0,0 +1,28 @@
+package com.springboot.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+}
