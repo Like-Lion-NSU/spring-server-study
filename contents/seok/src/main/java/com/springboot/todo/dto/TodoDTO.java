a68783dada50212dd5651b0e961808fc4a38c9d8
kws9208 <kws9208@gmail.com>
2023-02-07 21:49:04 +0900

    [Feat] todo layer 구현

---
+++ contents/seok/src/main/java/com/springboot/todo/dto/TodoDTO.java
@@ -0,0 +1,33 @@
+package com.springboot.todo.dto;
+
+import com.springboot.todo.entity.Todo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDTO {
+    private Long id;
+    private String item;
+    private String userId;
+    private boolean isDone;
+
+    public TodoDTO(Todo todo){
+        this.id = todo.getId();
+        this.item = todo.getItem();
+        this.userId = todo.getUser().getUserId();
+        this.isDone = todo.isDone();
+    }
+
+    public static Todo toEntity(TodoDTO dto){
+        return Todo.builder()
+                .id(dto.getId())
+                .item(dto.getItem())
+                .isDone(dto.isDone())
+                .build();
+    }
+}
