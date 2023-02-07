1a1255eb124f9979a5d436fc1a8de1ebbd997988
kws9208 <kws9208@gmail.com>
2023-02-07 21:49:04 +0900

    Feat: todo layer 구현

--- contents/seok/src/main/java/com/springboot/todo/repository/TodoRepository.java
+++ contents/seok/src/main/java/com/springboot/todo/repository/TodoRepository.java
@@ -1,9 +1,13 @@
 package com.springboot.todo.repository;
 
 import com.springboot.todo.entity.Todo;
+import com.springboot.todo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findByUser(User user);
 }
