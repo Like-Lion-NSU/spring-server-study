8ec32c69b24a4eb9d3fb73a50752098c945b9ca5
kws9208 <kws9208@gmail.com>
2023-02-13 21:21:54 +0900

    [Refactor] swagger jwt 인증 사용

--- contents/seok/src/main/java/com/springboot/todo/config/OpenApiConfig.java
+++ contents/seok/src/main/java/com/springboot/todo/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.springboot.todo.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +18,16 @@ public class OpenApiConfig {
                 .title("Todo API Document")
                 .version("v1.0.0")
                 .description("Todo API 명세서");
-        return new OpenAPI().info(info);
+
+        String jwtSchemeName = "bearerAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+        return new OpenAPI().info(info).addSecurityItem(securityRequirement).components(components);
     }
 }
