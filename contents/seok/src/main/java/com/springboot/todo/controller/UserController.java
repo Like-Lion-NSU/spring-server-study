5b0240085c21ef7fb8ed7c45eeae408f4afe7ffa
kws9208 <kws9208@gmail.com>
2023-02-13 19:36:34 +0900

    [Feat] JWT 생성 및 반환 구현

--- contents/seok/src/main/java/com/springboot/todo/controller/UserController.java
+++ contents/seok/src/main/java/com/springboot/todo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springboot.todo.controller;
 import com.springboot.todo.dto.ResponseDTO;
 import com.springboot.todo.dto.UserDTO;
 import com.springboot.todo.entity.User;
+import com.springboot.todo.security.TokenProvider;
 import com.springboot.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
@@ -35,15 +39,18 @@ public class UserController {
             ResponseDTO response = ResponseDTO.builder().error(e.getMessage()).build();
             return ResponseEntity.badRequest().body(response);
         }
+
     }
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
         User user = userService.getByCredentials(userDTO.getUserId(), userDTO.getPassword());
         if(user!=null){
+            final String token = tokenProvider.createToken(user);
             UserDTO responseUserDTO = UserDTO.builder()
                     .userId(user.getUserId())
                     .id(user.getId())
+                    .token(token)
                     .build();
             return ResponseEntity.ok().body(responseUserDTO);
         }else{
