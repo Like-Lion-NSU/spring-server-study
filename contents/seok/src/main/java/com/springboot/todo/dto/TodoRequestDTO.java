e207715907639fcc9247f931d283d1cc57619542
kws9208 <kws9208@gmail.com>
2023-02-23 16:43:36 +0900

    [Refactor] DTO 수정

---
+++ contents/seok/src/main/java/com/springboot/todo/dto/TodoRequestDTO.java
@@ -0,0 +1,15 @@
+package com.springboot.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoRequestDTO {
+    private String item;
+    private boolean isDone;
+}
---
+++ contents/seok/src/main/java/com/springboot/todo/dto/UserRequestDTO.java
@@ -0,0 +1,16 @@
+package com.springboot.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDTO {
+    private String userId;
+    private String password;
+    private String userName;
+}
