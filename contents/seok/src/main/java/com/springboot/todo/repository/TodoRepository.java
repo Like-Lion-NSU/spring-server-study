a26a9bd501f65e7dddd4e12fddb263af7ebb8744
kws9208 <kws9208@gmail.com>
2023-02-23 16:43:53 +0900

    [Refactor] 레포지토리 수정

--- contents/seok/src/main/java/com/springboot/todo/repository/TodoRepository.java
+++ contents/seok/src/main/java/com/springboot/todo/repository/TodoRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByUser(User user);
+    List<Todo> findByUser_Id(Long id);
 }
