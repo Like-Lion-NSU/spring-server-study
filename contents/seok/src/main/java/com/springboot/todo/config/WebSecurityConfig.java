0941be0fb8e877c821a5344cef507b9f0788ea79
kws9208 <kws9208@gmail.com>
2023-02-23 16:44:53 +0900

    [Refactor] 오타 수정

--- contents/seok/src/main/java/com/springboot/todo/config/WebSecurityConfig.java
+++ contents/seok/src/main/java/com/springboot/todo/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.springboot.todo.config;
 
 import com.springboot.todo.security.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
