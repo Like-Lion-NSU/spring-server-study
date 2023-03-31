61a8acf29e4966b04e554723fd7b61d1753ccd08
kws9208 <kws9208@gmail.com>
2023-03-31 22:07:50 +0900

    [Refactor] spring security 코드 리팩토링

---
+++ contents/seok/src/main/java/com/springboot/todo/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.springboot.todo.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.todo.dto.EntryPointErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final Logger logger = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        logger.info("[commence] 인증 실패로 response.sendError 발생");
+
+        EntryPointErrorResponse entryPointErrorResponse = new EntryPointErrorResponse();
+        entryPointErrorResponse.setMsg("인증이 실패하였습니다.");
+
+        response.setStatus(401);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(entryPointErrorResponse));
+    }
+}
